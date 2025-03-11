383c5e021fde04de930f580d53347e181d21cf36
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-03-11 07:25:54 +0200

    change app logo and make splash screen

--- app/src/main/java/com/example/bloglyapp/MainActivity.kt
+++ app/src/main/java/com/example/bloglyapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.bloglyapp.presentation.navigation.NavGraph
 import com.example.bloglyapp.presentation.theme.BloglyAppTheme
@@ -16,6 +17,7 @@ import com.example.bloglyapp.presentation.theme.BloglyAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             BloglyAppTheme {
